9526221b4b22396f6e9ba901639f0ed5e6d33b36
danodlr <jose.ayala@encora.com>
2021-12-04 19:39:06 -0600

    Anser = 1139

--- day1/sonar_sweep.go
+++ day1/sonar_sweep.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	lines, err := readLines("input.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	measurements_larger := 0
+	var previous int
+	for i, line := range lines {
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatalf("printing loop: %d -> %s", i, err)
+		}
+		if i == 0 {
+			previous = number
+		} else if previous < number {
+			measurements_larger += 1
+			previous = number
+		} else {
+			previous = number
+		}
+	}
+	fmt.Println(measurements_larger)
+
 }
